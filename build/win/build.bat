ea039b57889cc222bc4377616e73c76360eae810
Shark Attack <sharkattack3020@gmail.com>
2025-08-25 08:27:01 +0300

    Build script (Windows)

---
+++ build/win/build.bat
@@ -0,0 +1,24 @@
+:: Windows Build
+@echo off
+
+:: Save current directory and move two levels up
+pushd .
+cd ..\..
+
+:: Set build type
+set BUILD_TYPE=win
+
+:: Set optimization flags
+set OPTIMIZATIONS=-Os --no-exception-messages --no-debug-info --no-pie --no-stacktrace-data
+
+:: Initialize global variables sets
+call build\globalSets.bat
+
+BFLAT build ^
+    -r "%SHARED_PATH%\System.CommandLine.dll" ^
+    -o "%OUTPUT_PATH%" ^
+    %OPTIMIZATIONS% ^
+    @"%SOURCE_PATH%"
+
+:: Restore previous directory
+popd
