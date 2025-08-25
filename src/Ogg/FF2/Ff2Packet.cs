1758d4c3300dbf67a48f84cd556f3265958c5e97
Shark Attack <sharkattack3020@gmail.com>
2025-08-25 08:27:00 +0300

    FF2 packet structure

---
+++ src/Ogg/FF2/Ff2Packet.cs
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+// Project: FF2Theora
+// Author: Shark Attack (https://github.com/SharkAttack276)
+// License: GPL-3.0-or-later
+//
+// This file is part of FF2Theora.
+//
+// You are free to modify and redistribute this code under the terms of
+// the GNU General Public License, version 3 or later.
+//
+// If you modify this file, YOU MUST:
+//   1. Retain attribution to the original author.
+//   2. Clearly state what changes you made.
+//
+// See the LICENSE file in the root of this repository for more details.
+namespace FF2Theora.Ogg;
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public struct Ff2Packet
+{
+    public int PacketSize;
+    public int StartOfStreamFlag;
+    public int EndOfStreamFlag;
+    public long Granule;
+    public int PacketNumber;
+    public int SerialNumber;
+    public override string ToString() =>
+        $"{nameof(Ff2Packet)} {PacketNumber}";
+}
