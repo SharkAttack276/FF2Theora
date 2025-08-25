19aafe8a2ca840b7ae91f9efd84232cdef9cfd09
Shark Attack <sharkattack3020@gmail.com>
2025-08-25 08:26:57 +0300

    Logger extension methods

---
+++ src/Extensions/LoggerExtension.cs
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+// Project: FF2Theora
+// Author: Shark Attack (https://github.com/SharkAttack276)
+// License: GPL-3.0-or-later
+//
+// This file is part of FF2Theora.
+//
+// You are free to modify and redistribute this code under the terms of
+// the GNU General Public License, version 3 or later.
+//
+// If you modify this file, YOU MUST:
+//   1. Retain attribution to the original author.
+//   2. Clearly state what changes you made.
+//
+// See the LICENSE file in the root of this repository for more details.
+namespace FF2Theora.Extensions;
+
+// Logger Extension
+
+public static partial class Extension
+{
+	public static void Log(string Message, LogType LogType)
+	{
+		ConsoleColor previousColor = Console.ForegroundColor;
+		Console.ForegroundColor = (ConsoleColor)LogType;
+		Console.Error.WriteLine($"	{LogType}: {Message}");
+		Console.ForegroundColor = previousColor;
+	}
+}
