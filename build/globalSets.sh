4f550c688bc2f1609b2170895298725888d08561
Shark Attack <sharkattack3020@gmail.com>
2025-08-25 08:27:01 +0300

    Global sets script (Linux)

---
+++ build/globalSets.sh
@@ -0,0 +1,17 @@
+# Program Name Output
+PROGRAM_NAME="FF2Theora"
+
+# Library Paths
+SHARED_PATH="shared"
+
+# Base root path
+ROOT_PATH="BUILD"
+
+# Directory output path
+DIRECTORY_PATH="$ROOT_PATH/$BUILD_TYPE"
+
+# Output generated path
+OUTPUT_PATH="$DIRECTORY_PATH/$PROGRAM_NAME"
+
+# Source files path
+SOURCE_PATH="$ROOT_PATH/SRC"
