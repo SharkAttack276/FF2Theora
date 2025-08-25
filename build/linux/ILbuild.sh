41eedb378c62e888f82333c67bcbc8a046ad7c3e
Shark Attack <sharkattack3020@gmail.com>
2025-08-25 08:27:02 +0300

    IL build script (Linux)

---
+++ build/linux/ILbuild.sh
@@ -0,0 +1,23 @@
+# Linux/Unix Build
+
+# Save current directory and move two levels up
+pushd . > /dev/null
+cd "$(dirname "$0")/../.."
+
+# Set build type
+BUILD_TYPE="linux"
+
+# Set optimization flags
+OPTIMIZATIONS="--no-debug-info"
+
+# Initialize global variable sets
+source build/globalSets.sh
+
+BFLAT build-il \
+  -r "$SHARED_PATH/System.CommandLine.dll" \
+  -o "$OUTPUT_PATH" \
+  $OPTIMIZATIONS \
+  @"$SOURCE_PATH"
+
+# Restore previous directory
+popd > /dev/null
