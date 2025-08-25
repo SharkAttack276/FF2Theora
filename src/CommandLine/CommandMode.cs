c4f00ea805a38e953152c3114157c0832bc57e8e
Shark Attack <sharkattack3020@gmail.com>
2025-08-25 08:26:56 +0300

    Command mode enumeration

---
+++ src/CommandLine/CommandMode.cs
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+// Project: FF2Theora
+// Author: Shark Attack (https://github.com/SharkAttack276)
+// License: GPL-3.0-or-later
+//
+// This file is part of FF2Theora.
+//
+// You are free to modify and redistribute this code under the terms of
+// the GNU General Public License, version 3 or later.
+//
+// If you modify this file, YOU MUST:
+//   1. Retain attribution to the original author.
+//   2. Clearly state what changes you made.
+//
+// See the LICENSE file in the root of this repository for more details.
+namespace FF2Theora.CommandLine;
+
+public enum CommandMode
+{
+    Default,
+    Decode
+}
